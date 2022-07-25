7591be94624eab94a9f35c1fd9a7d07df16fe211
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-07-25 17:03:16 +0530

    routes

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import React,{Component} from "react";
 import { Card, Button} from "semantic-ui-react";
 import factory from '../ethereum/factory';
 import Layout from '../components/Layout'
+import {Link} from '../routes';
+
 class CampaignIndex extends Component{
     static async getInitialProps(){
         const campaigns= await factory.methods.getDeployedCampaigns().call();
@@ -11,7 +13,10 @@ class CampaignIndex extends Component{
       const items=this.props.campaigns.map(address=>{
         return {
             header: address,
-            description:<a>View Campaigns</a>,
+            description:(
+            <Link route={`/campaigns/${address}`}>
+            <a>View Campaigns</a>
+            </Link>),
             fluid:true
         };
       }); 
@@ -27,13 +32,16 @@ class CampaignIndex extends Component{
     href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
   />
   <h3> Open Campaigns</h3>
-        
+        <Link route="/campaigns/new">
+        <a>
         <Button
             floated="right"
             content="Create Campaign"
             icon="add circle"
             primary
         />
+        </a>
+        </Link>
         {this.renderCampaigns()}
         </div>
         </Layout>
