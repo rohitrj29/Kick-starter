a823ba4cba6c34a781290596721acc348573fadc
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-07-25 14:44:26 +0530

    open campaign

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,9 @@
 import React from "react";
- 
+import Header from './Header'; 
+
 const Layout = (props) => {
   return(<div>
-    <h1>I am a header</h1>
+    <Header/>
     {props.children}
     <h1>I am footer</h1>
   </div>)
--- pages/index.js
+++ pages/index.js
@@ -27,12 +27,14 @@ class CampaignIndex extends Component{
     href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
   />
   <h3> Open Campaigns</h3>
-        {this.renderCampaigns()}
+        
         <Button
+            floated="right"
             content="Create Campaign"
             icon="add circle"
             primary
         />
+        {this.renderCampaigns()}
         </div>
         </Layout>
     }
